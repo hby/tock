7ef093550bb2f5513c4e26fb2ab94fb4fd73756b
Bret Young <hby@acm.org>
2020-09-08 21:44:49 -0500

    Some doc strings

--- src/com/inferstructure/tock.clj
+++ src/com/inferstructure/tock.clj
@@ -15,20 +15,24 @@
 ;;
 
 (defmethod digit :tock-builtin/fn
+  ;; a literal digit function
   [_ f]
   (im/->Digit f nil))
 
 (defmethod digit :tock-builtin/seq
+  ;; digit function is constructed that always returns the given sequence
   [_ s]
   (im/->Digit (constantly s) nil))
 
 (defn- suffix
+  "Returns true if sv is a suffix of v."
   [v sv]
   (if (<= (count sv) (count v))
     (reduce #(and % %2) (map = (reverse v) (reverse sv)))
     false))
 
 (defmethod digit :tock-builtin/kvs
+  ;;
   [_ kvs]
   (im/->Digit (fn [& vs]
                 (let [[_ v] (first (filter #(suffix vs (first %)) kvs))]
@@ -39,41 +43,55 @@
 ;; Create Counters
 ;;
 
-(defn counter [ctrs]
+(defn counter
+  "Create and return a Counter from a sequence of Counters or Digits."
+  [ctrs]
   (reduce (fn [cs c]
             (im/->Counter c cs))
           nil
           ctrs))
 
 ;;
-;; Sequences
-;;
-
-(defn counter-seq
-  [ctr]
-  (lazy-seq
-    (cons ctr (counter-seq (im/tick ctr)))))
-
-(defn value-seq
-  [ctr]
-  (->> ctr
-       counter-seq
-       (map im/value)
-       (take-while #(not-empty %))))
-
-;;
 ;; Digit and Counter API
 ;;
 
 (defn start
+  "Returns a started counter or digit.
+  Optionally takes a starting value and sequence of higher order values."
   ([ic] (im/start ic))
   ([ic sv] (im/start ic sv))
   ([ic sv hovs] (im/start ic sv hovs)))
 
 (defn tick
+  "Returns a given counter advanced one 'count'.
+  Optionally takes a sequence of higher order values."
   ([ic] (im/tick ic))
   ([ic hovs] (im/tick ic hovs)))
 
 (defn value
+  "Returns the value of the given counter."
   [ic]
   (im/value ic))
+
+;;
+;; Sequences
+;;
+
+(defn counter-seq
+  "Return a lazy sequence of Counters ctr, (tick ctr), (tick (tick ctr)), ...
+  This will always be an infinite sequence since once (value ctr) is empty
+  then (tick ctr) will continue to produce an empty valued counter."
+  [ctr]
+  (lazy-seq
+    (cons ctr (counter-seq (im/tick ctr)))))
+
+(defn value-seq
+  "Return a lazy sequence of counters values
+   (value ctr), (value (tick ctr)), (value (tick (tick ctr))), ...
+   The sequence ends when the underlying counter sequence produces
+   an empty value (that is, the counter runs out)"
+  [ctr]
+  (->> ctr
+       counter-seq
+       (map im/value)
+       (take-while #(not-empty %))))
